ea1b18f82d77a712d2ff56ca21e12bbb22450fdf
Jean Roudy Pierre <JPierre1@KMB.UIO>
2021-10-21 15:30:01 -0500

    paginas listas para poner funcionalidad

---
+++ demo/vertical-default-dark/dashboardPromesaServicio.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require './head.html' ?>
+</head>
+
+<body>
+    <div class="container-scroller">
+        <?php require './navbar.html' ?>
+        <div class="container-fluid page-body-wrapper">
+            <?php require './menu.html' ?>
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    BODY PROMESA AQUI
+                </div>
+                <?php require './footer.html' ?>
+            </div>
+        </div>
+    </div>
+    <?php require './scripts.html' ?>
+</body>
+
+</html>
